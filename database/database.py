a76ff50088a43c5a16fd54cbc4bc0608528afb9b
JorgeDDuran <jorgerena3@gmail.com>
2023-10-26 18:56:27 -0500

    nuevos archivos estructura

--- database/database.py
+++ database/database.py
@@ -1,8 +1,9 @@
 import os
 import mysql.connector
+from dotenv import load_dotenv
+from model.user import User
 
 # Carga las variables de entorno desde el archivo .env
-from dotenv import load_dotenv
 load_dotenv()
 
 # Obtiene los valores de las variables de entorno
@@ -23,3 +24,19 @@ db_connection = mysql.connector.connect(
 
 # Crea un cursor para ejecutar consultas SQL
 db_cursor = db_connection.cursor()
+
+def insert_user(user):
+    try:
+        # Realiza la inserción en la base de datos
+        query = "INSERT INTO Persona (Nombre, Apellido, Fecha_Nacimiento, Genero, Direccion, Telefono, Correo_Electronico, Numero_Identificacion) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (user.nombre, user.apellido, user.fecha_nacimiento, user.genero, user.direccion, user.telefono, user.correo_electronico, user.numero_identificacion)
+
+        db_cursor.execute(query, values)
+        db_connection.commit()
+
+        # Retorna el ID del usuario recién insertado
+        return db_cursor.lastrowid
+    except Exception as e:
+        # Manejo de errores
+        print("Error al insertar usuario:", str(e))
+        db_connection.rollback()
