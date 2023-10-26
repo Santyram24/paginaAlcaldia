9c5ab5d5ffff315cc49c9a714c2bfc08a42e16a6
JorgeDDuran <jorgerena3@gmail.com>
2023-10-26 18:50:21 -0500

    nuevos archivos estructura

---
+++ model/reconocimienti_log_model.py
@@ -0,0 +1,9 @@
+class RecognitionLog:
+    def __init__(self, id, id_persona, fecha_hora_entrada, fecha_hora_salida, id_sistema_reconocimiento):
+        self.id = id
+        self.id_persona = id_persona
+        self.fecha_hora_entrada = fecha_hora_entrada
+        self.fecha_hora_salida = fecha_hora_salida
+        self.id_sistema_reconocimiento = id_sistema_reconocimiento
+
+# Puedes agregar métodos adicionales según tus necesidades.
