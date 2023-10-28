533b80accfe29b5497b56d802b4d0a4eeb3e2542
CristianNino <crnino@ucundinamarca.edu.co>
2023-10-28 16:41:06 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from view.view import RegistrationApp
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = RegistrationApp()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
