6ccd9814ba5c1359d8dd2192754869282a6034b2
Cristian Gonzalez <145730165+CristianGonzalez250@users.noreply.github.com>
2023-10-28 16:45:49 -0500

    Update user_model.py

--- model/user_model.py
+++ model/user_model.py
@@ -1,13 +1,6 @@
+# user_model.py
 class User:
-    def __init__(self, id, nombre, apellido, fecha_nacimiento, genero, direccion, telefono, correo_electronico, numero_identificacion):
-        self.id = id
-        self.nombre = nombre
-        self.apellido = apellido
-        self.fecha_nacimiento = fecha_nacimiento
-        self.genero = genero
-        self.direccion = direccion
-        self.telefono = telefono
-        self.correo_electronico = correo_electronico
-        self.numero_identificacion = numero_identificacion
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-# Puedes agregar métodos adicionales según tus necesidades.
