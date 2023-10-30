c35473bc8d6d01f479567282c03ce3af60ed98fa
Cristian Gonzalez <145730165+CristianGonzalez250@users.noreply.github.com>
2023-10-30 16:18:22 -0500

    Update a.py

--- services/a.py
+++ services/a.py
@@ -1,21 +1,38 @@
 import cv2
-import dlib
+import os
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
 
-predictor_path = "shape_predictor_68_face_landmarks.dat"
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(predictor_path)
+def detect_faces(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    return faces
 
-image = cv2.imread("person.jpg")
+def prepare_training_data(data_folder_path):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-dets = detector(image, 1)
-for k, d in enumerate(dets):
-    shape = predictor(image, d)
-    points = np.empty([68, 2], dtype=int)
-    for i in range(68):
-        points[i] = (shape.part(i).x, shape.part(i).y)
-    for p in range(68):
-        cv2.circle(image, (points[p][0], points[p][1]), 1, (0, 255, 0), -1, 8, 0)
+    face_samples = []
+    ids = []
 
+    for root, dirs, files in os.walk(data_folder_path):
+        for file in files:
+            if file.endswith("png") or file.endswith("jpg"):
+                path = os.path.join(root, file)
+                id = int(os.path.basename(root).replace("person", ""))
+                print(f"Id: {id}")
 
-cv2.imshow("Fa")
+                img = cv2.imread(path)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+                faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+                for (x, y, w, h) in faces:
+                    face_samples.append(gray[y:y+h, x:x+w])
+                    ids.append(id)
+
+    return face_samples, ids
+
+def recognize_faces(test_img, face_samples, ids, svm_model):
+    face_locations
