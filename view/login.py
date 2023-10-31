ea7d5ef79279cd108f701b71a5ba8fbd301a6a51
Cristian Gonzalez <145730165+CristianGonzalez250@users.noreply.github.com>
2023-10-31 13:53:35 -0500

    Update login.py

--- view/login.py
+++ view/login.py
@@ -1,96 +1,41 @@
 import tkinter as tk
-from tkinter import ttk
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtGui import QFont  # Importa QFont
-from PyQt5.QtCore import Qt  # Importa Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout,QFrame, QWidget,QSplitter
+from tkinter import messagebox
+import os, sys
 
-
-def signup():
-    # Agrega aquí la lógica para el registro facial
-    print("Registro Facial")
+def registrate():
+    messagebox.showinfo('Registrate', 'Por favor, regístrate antes de continuar')
 
 def login():
-    # Agrega aquí la lógica para el inicio de sesión
-    print("Inicio de Sesión")
+    username = entry_username.get()
+    password = entry_password.get()
 
-def pantalla_principal():
-    app = QApplication(sys.argv)
-    ventana = QMainWindow()
-    ventana.setWindowTitle("Registro")
-    ventana.setGeometry(100, 100, 1000, 600)  # Aumenta el ancho de la ventana
+    if username == 'user' and password == 'pass':
+        messagebox.showinfo('Login', 'Ingreso correcto')
+    else:
+        messagebox.showerror('Login', 'Usuario o contraseña incorrectos')
 
-    layout = QVBoxLayout()
+app = tk.Tk()
+app.title('Login')
+app.geometry('400x260')
+app.configure(bg="#025E73")
 
-    left_widget = QWidget()  # Widget contenedor para el grupo de registro
-    left_layout = QVBoxLayout(left_widget)
-    usuario_label = QLabel("Usuario *")
-    usuario_entrada = QLineEdit()
-    usuario_label.setBuddy(usuario_entrada)
+label_username = tk.Label(app, text='Usuario')
+label_username.pack(padx=0, pady=5)
 
-    contra_label = QLabel("Contraseña *")
-    contra_entrada = QLineEdit()
-    contra_label.setBuddy(contra_entrada)
+entry_username = tk.Entry(app)
+entry_username.pack(padx=20, pady=20)
+# entry_username.place(x=25, y=10)
 
-    boton_registro_facial = QPushButton("Registro Facial")
-    boton_registro_facial.setStyleSheet("background-color: #3498db; color: white; border-radius: 5px; height: 50px;")
-    boton_registro_facial.clicked.connect(signup)
+label_password = tk.Label(app, text='Contraseña')
+label_password.pack()
 
-    left_layout.addWidget(QLabel("Registro facial: debe asignar un usuario y contraseña:"))
-    left_layout.addWidget(usuario_label)
-    left_layout.addWidget(usuario_entrada)
-    left_layout.addWidget(contra_label)
-    left_layout.addWidget(contra_entrada)
-    left_layout.addWidget(boton_registro_facial)
+entry_password = tk.Entry(app, show='*')
+entry_password.pack(padx=20, pady=20)
 
-    right_widget = QWidget()  # Widget contenedor para el grupo de inicio de sesión
-    right_layout = QVBoxLayout(right_widget)
-    usuariologueo_label = QLabel("Usuario *")
-    usuariologueo_entrada = QLineEdit()
-    usuariologueo_label.setBuddy(usuariologueo_entrada)
+button_login = tk.Button(app, text='Ingresar', command=login)
+button_login.pack()
 
-    contralogueo_label = QLabel("Contraseña *")
-    contralogueo_entrada = QLineEdit()
-    contralogueo_label.setBuddy(contralogueo_entrada)
+button_extra = tk.Button(app, text='Registrate', command=registrate)
+button_extra.pack(padx=20, pady=20)
 
-    boton_logueo = QPushButton("Inicio de Sesión")
-    boton_logueo.setStyleSheet("background-color: #27ae60; color: white; border-radius: 5px; height: 50px;")
-    boton_logueo.clicked.connect(login)
-
-    right_layout.addWidget(QLabel("Inicio de sesion:"))
-    right_layout.addWidget(usuariologueo_label)
-    right_layout.addWidget(usuariologueo_entrada)
-    right_layout.addWidget(contralogueo_label)
-    right_layout.addWidget(contralogueo_entrada)
-    right_layout.addWidget(boton_logueo)
-
-
-
-    # Utiliza un QFrame como separador
-    separator = QFrame()
-    separator.setFrameShape(QFrame.VLine)  # Separador vertical
-    separator.setFrameShadow(QFrame.Sunken)
-
-    # Utiliza un QSplitter para la separación horizontal
-    splitter = QSplitter()
-    splitter.addWidget(left_widget)
-    splitter.addWidget(separator)  # Agrega el separador
-    splitter.addWidget(right_widget)
-    left_widget.setMinimumWidth(200)
-    right_widget.setMinimumWidth(200)
-
-
-    layout.addWidget(splitter)
-    # Ajusta los tamaños iniciales de los widgets (izquierda, separador, derecha)
-    splitter.setSizes([1, 1, 1])
-
-
-    central_widget = QWidget()
-    central_widget.setLayout(layout)
-    ventana.setCentralWidget(central_widget)
-
-    ventana.show()
-    sys.exit(app.exec_())
-
-pantalla_principal()
+app.mainloop()
