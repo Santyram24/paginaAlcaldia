d23b22dcf948a05b83c9470f512ad4cc56f0456e
sergiolix <sergiolix8@gmail.com>
2023-11-03 15:54:05 -0500

    reconocer caras

--- view/view.py
+++ view/view.py
@@ -1,5 +1,8 @@
 import sys
 import cv2
+import os
+import imutils
+import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
 from model.user_model import User
 from model.reconocimiento_model import RecognitionLog
@@ -113,10 +116,42 @@ class RegistrationApp(QMainWindow):
             file.write(f"Usuario: {username}, Contraseña: {password}\n")
 
     def capture_and_save_facial_image(self, username):
-        image_path = capture_facial_image()
-        if image_path:
-            recognition_log = RecognitionLog(username, image_path)
-            self.save_recognition_log(recognition_log)
+        personName = username
+        dataPath = 'C:/Users/Sergio/Desktop/Reconocimiento Facial/Data' 
+        personPath = dataPath + '/' + personName
+
+        if not os.path.exists(personPath):
+	        print('Carpeta creada: ',personPath)
+	        os.makedirs(personPath)
+
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+        faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+        count = 0
+        while True:
+
+            ret, frame = cap.read()
+            if ret == False: break
+            frame =  imutils.resize(frame, width=640)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            auxFrame = frame.copy()
+
+            faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+            for (x,y,w,h) in faces:
+                cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+                rostro = auxFrame[y:y+h,x:x+w]
+                rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
+                cv2.imwrite(personPath + '/rotro_{}.jpg'.format(count),rostro)
+                count = count + 1
+            cv2.imshow('frame',frame)
+
+            k =  cv2.waitKey(1)
+            if k == 27 or count >= 50:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
 
     def save_recognition_log(self, recognition_log):
         username = recognition_log.username
@@ -130,52 +165,44 @@ class RegistrationApp(QMainWindow):
         username = self.auth_username_input.text()
         password = self.auth_password_input.text()
         
-        haar_fila = 'haarcascade_frontalface_default.xml'
-        face_cascade = cv2.CascadeClassifier(haar_fila)
-        webcam = cv2.VideoCapture(0)
+        dataPath = 'C:/Users/Sergio/Desktop/Reconocimiento Facial/Data'
+        imagePaths = os.listdir(dataPath)
+        print('imagePaths=',imagePaths)
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+        face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+        face_recognizer.read('modeloLBPHFace.xml')
+        faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
         while True:
-            (_, im) = webcam.read()
-            gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-            faces =  face_cascade.detectMultiScale(gray)
-    
-            for (x, y, w, h) in faces:
-                cv2.rectangle(im, (x,y), (x+w,y+h), (255, 0,0),2)
-            cv2.imshow('OpenCV', im)
-        
-            key = cv2.waitKey(10)
-            if key == 27:
-                break
-        
-  
-        if not username or not password:
-            return  # Validación de entrada
+                ret,frame = cap.read()
+                if ret == False: break
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                auxFrame = gray.copy()
 
-        if self.verify_user_credentials(username, password):
-            recognition_log = self.get_recognition_log_by_username(username)
-            
-            if recognition_log:
-                cap = cv2.VideoCapture(0)  # Iniciar la captura de la cámara
+                faces = faceClassif.detectMultiScale(gray,1.3,5)
 
-                while True:
-                    ret, frame = cap.read()
-                    if not ret:
-                        break
+                for (x,y,w,h) in faces:
+                    rostro = auxFrame[y:y+h,x:x+w]
+                    rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+                    result = face_recognizer.predict(rostro)
 
-                    if compare_facial_images(frame, recognition_log.image_path):
-                        self.show_authentication_result("Autenticación facial exitosa.")
-                        break
+                    cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+                    
+                    if result[1] < 70:
+                        cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+                        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
                     else:
-                        self.show_authentication_result("Fallo en la autenticación facial.")
+                        cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+                        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+                    
+                cv2.imshow('frame',frame)
+                k = cv2.waitKey(1)
+                if k == 27:
+                    break
 
-                    # Mostrar el frame de la cámara en la interfaz gráfica
-                    self.display_camera_frame(frame)
-
-                cap.release()  # Detener la captura de la cámara
-            else:
-                self.show_authentication_result("Fallo en la recuperación del registro de reconocimiento.")
-        else:
-            self.show_authentication_result("Credenciales de usuario no válidas.")
+        cap.release()
+        cv2.destroyAllWindows()
 
     def verify_user_credentials(self, username, password):
         # Esta función debería consultar una base de datos o archivo para verificar las credenciales del usuario.
@@ -196,6 +223,7 @@ class RegistrationApp(QMainWindow):
     def display_camera_frame(self, frame):
         # Muestra el frame de la cámara en la interfaz gráfica (por ejemplo, en un QLabel)
         pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
