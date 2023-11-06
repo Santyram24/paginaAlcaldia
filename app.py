f4b754ad440e6e66c9f4d88cd4741a09f8532644
JorgeDDuran <jorgerena3@gmail.com>
2023-11-06 08:59:44 -0500

    Update app.py
    Agrego uso del modelo de entrenamiento

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication 
 from view.view import RegistrationApp
-
+from model.training import train_model
 
 def main():
     app = QApplication(sys.argv)
     window = RegistrationApp()
     window.show()
+    train_model('C:/Users/jddur/OneDrive/Desktop/FOTOS RECON', 'modeloLBPHFace.xml')  # Llama a train_model
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
+    
+    
+
