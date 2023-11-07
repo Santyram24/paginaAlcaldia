4ae785168534d0f1e4c54ada9fe9ce59713f8672
CristianNino <crnino@ucundinamarca.edu.co>
2023-11-07 18:13:08 -0500

    Update entrenandoRF.py

--- entrenandoRF.py
+++ entrenandoRF.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import numpy as np
 
-dataPath = 'C:/Users/jddur/OneDrive/Desktop/FOTOS RECON' 
+dataPath = 'FotosR' 
 peopleList = os.listdir(dataPath)
 print('Lista de personas: ', peopleList)
 
