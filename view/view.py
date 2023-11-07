d17c72d2893678e455905ee2b2193d11994402f7
JorgeDDuran <jorgerena3@gmail.com>
2023-11-07 17:22:28 -0500

    Update view.py
    update

--- view/view.py
+++ view/view.py
@@ -3,11 +3,13 @@ import cv2
 import os
 import imutils
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QComboBox, QMessageBox
+from model.training import train_model
 from model.user_model import User
 from model.reconocimiento_model import RecognitionLog
 from services.reconocimiento_facial import compare_facial_images, capture_facial_image
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 from PyQt5.QtGui import QPixmap
 
 class RegistrationApp(QMainWindow):
@@ -19,16 +21,16 @@ class RegistrationApp(QMainWindow):
         self.setWindowTitle("Registro y Autenticación con Reconocimiento Facial")
         self.setGeometry(100, 100, 600, 400)
 
-        # Configura el fondo azul utilizando una hoja de estilo
+    # Configura el fondo azul utilizando una hoja de estilo
         style = """
             QMainWindow {
                 background-color: #3498db;
             }
-            
+        
             QLabel {
                 color: white;
             }
-           
+       
             QPushButton {
                 background-color: #27ae60;
                 color: white;
@@ -49,7 +51,7 @@ class RegistrationApp(QMainWindow):
 
         layout = QVBoxLayout()
 
-        # Widgets para el registro
+    # Widgets para el registro
         username_label = QLabel("Nombre de Usuario")
         self.username_input = QLineEdit()
         password_label = QLabel("Contraseña")
@@ -59,19 +61,44 @@ class RegistrationApp(QMainWindow):
         register_button = QPushButton("Registrar")
         register_button.clicked.connect(self.register_user)
 
-        # Crear un QLabel para la imagen
+    # Crear un QLabel para la imagen
         image_label = QLabel(self)
         pixmap = QPixmap('view/img/Granada.jpg')  # Asegúrate de tener la imagen en la ubicación correcta
         pixmap = pixmap.scaled(100, 120, Qt.KeepAspectRatio)  # Redimensionar la imagen
         image_label.setPixmap(pixmap)
 
-        # Crear un layout horizontal para la imagen
+    # Crear un layout horizontal para la imagen
         image_layout = QVBoxLayout()
         image_layout.addStretch(1)  # Espacio en blanco a la izquierda de la imagen
         image_layout.addWidget(image_label)  # Agregar la imagen
         image_layout.addStretch(1)  # Espacio en blanco a la derecha de la imagen
 
-        # Widgets para la autenticación
+    # Agregar elementos de registro al layout
+        layout.addWidget(username_label)
+        layout.addWidget(self.username_input)
+        layout.addWidget(password_label)
+        layout.addWidget(self.password_input)
+        
+        
+        role_label = QLabel("Rol")
+        self.role_combo = QComboBox()
+        self.role_combo.addItem("Cliente")
+        self.role_combo.addItem("Funcionario")
+        self.role_combo.addItem("Trabajador")
+        role_layout = QVBoxLayout()
+        role_layout.addWidget(role_label)
+        role_layout.addWidget(self.role_combo)
+        layout.addLayout(role_layout)
+        layout.addWidget(register_button)
+        
+        
+        layout.addWidget(role_label)
+        layout.addWidget(self.role_combo)
+
+    # Agregar layout de imagen y elementos de autenticación al layout principal
+        layout.addLayout(image_layout)
+
+    # Widgets para la autenticación
         auth_username_label = QLabel("Nombre de Usuario")
         self.auth_username_input = QLineEdit()
         auth_password_label = QLabel("Contraseña")
@@ -80,44 +107,67 @@ class RegistrationApp(QMainWindow):
         authenticate_button = QPushButton("Ingresar")
         authenticate_button.clicked.connect(self.authenticate_user)
 
-        layout.addWidget(username_label)
-        layout.addWidget(self.username_input)
-        layout.addWidget(password_label)
-        layout.addWidget(self.password_input)
-        layout.addWidget(register_button)
-        
-        layout.addLayout(image_layout)
-        
         layout.addWidget(auth_username_label)
         layout.addWidget(self.auth_username_input)
         layout.addWidget(auth_password_label)
         layout.addWidget(self.auth_password_input)
         layout.addWidget(authenticate_button)
 
+    # Botón "Entrenar Modelo"
+        train_model_button = QPushButton("Entrenar Modelo")
+        train_model_button.clicked.connect(self.train_model)
+        layout.addWidget(train_model_button)  # Agrega el botón "Entrenar Modelo"
+
         central_widget.setLayout(layout)
+
+        
+    def train_model(self):
+        data_path = 'C:/Users/jddur/OneDrive/Desktop/FOTOS RECON'  # Ruta de tus datos de entrenamiento
+        model_path = 'modeloLBPHFace.xml'  # Ruta donde se guardará el modelo entrenado
+
+    # Llama a la función de entrenamiento desde model.training
+        train_model(data_path, model_path)
+
+    # Muestra un cuadro de diálogo con la información del entrenamiento
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowTitle("Entrenamiento Completado")
+
+    # Establece el estilo para el texto del cuadro de diálogo
+        msg.setStyleSheet("QLabel{ color: black; }")
+
+        msg.setText("El modelo ha sido entrenado con éxito.")
+        msg.setStandardButtons(QMessageBox.Ok)
+
+        result = msg.exec_()  # Muestra el cuadro de diálogo
+
+
+    
 
     def register_user(self):
         username = self.username_input.text()
         password = self.password_input.text()
+        role = self.role_combo.currentText()  
 
         if not username or not password:
             return  # Validación de entrada
 
-        user = User(username, password)
+        user = User(username, password, role)  # Pasa el rol como tercer argumento
         self.save_user_data(user)
         self.capture_and_save_facial_image(username)
 
     def save_user_data(self, user):
         username = user.username
         password = user.password
+        role = user.role  # Obtén el rol del objeto User
         user_data_file = "user_data.txt"
 
         with open(user_data_file, "a") as file:
-            file.write(f"Usuario: {username}, Contraseña: {password}\n")
+            file.write(f"Usuario: {username}, Contraseña: {password}, Rol: {role}\n")
 
     def capture_and_save_facial_image(self, username):
         personName = username
-        dataPath = 'C:/Users/Sergio/Desktop/Reconocimiento Facial/Data' 
+        dataPath = 'C:/Users/jddur/OneDrive/Desktop/FOTOS RECON' 
         personPath = dataPath + '/' + personName
 
         if not os.path.exists(personPath):
@@ -147,7 +197,7 @@ class RegistrationApp(QMainWindow):
             cv2.imshow('frame',frame)
 
             k =  cv2.waitKey(1)
-            if k == 27 or count >= 50:
+            if k == 27 or count >= 100:
                 break
 
         cap.release()
@@ -165,7 +215,7 @@ class RegistrationApp(QMainWindow):
         username = self.auth_username_input.text()
         password = self.auth_password_input.text()
         
-        dataPath = 'C:/Users/Sergio/Desktop/Reconocimiento Facial/Data'
+        dataPath = 'C:/Users/jddur/OneDrive/Desktop/FOTOS RECON'
         imagePaths = os.listdir(dataPath)
         print('imagePaths=',imagePaths)
         cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -216,9 +266,18 @@ class RegistrationApp(QMainWindow):
         return RecognitionLog(username, image_path)
 
     def show_authentication_result(self, message):
-        # Muestra el resultado de la autenticación en la interfaz gráfica
-        # Puedes usar un QLabel o QMessageBox para mostrar el mensaje.
-        pass
+        msg = QMessageBox()
+        msg.setWindowTitle("Resultado de la autenticación")
+
+        if message == "Autenticación exitosa":
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("¡Autenticación exitosa! El usuario ha sido reconocido.")
+        else:
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Autenticación fallida. Usuario desconocido o credenciales incorrectas.")
+
+        msg.exec_()
+
 
     def display_camera_frame(self, frame):
         # Muestra el frame de la cámara en la interfaz gráfica (por ejemplo, en un QLabel)
